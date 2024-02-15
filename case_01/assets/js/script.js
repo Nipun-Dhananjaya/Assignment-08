921728d2490bb1c8fea00f3bc4cee79df2465fcf
Nipun-Dhananjaya <nipundhananjaya967@gmail.com>
2024-02-15 10:29:30 +0530

    case 01 complete

--- case_01/assets/js/script.js
+++ case_01/assets/js/script.js
@@ -1,1 +1,19 @@
+let texts = ["A", "B", "C", "D", "E", "F"];
+let interval = null;
 
+function changeText() {
+    for (let i = 0; i < texts.length; i++) {
+        $(".text").eq(i).text(texts[i]);
+    }
+    texts.unshift(texts.pop())
+}
+$("#start").on('click', () => {
+    if (interval == null) {
+        interval =  setInterval(changeText, 1000);
+    }
+});
+
+$("#stop").on('click', () => {
+    clearInterval(interval);
+    interval = null;
+});
